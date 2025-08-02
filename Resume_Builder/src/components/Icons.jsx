a0a55802481e57ca2ec709588715f1d6358449f4
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-08-02 16:50:24 +0300

    Resume Builder: add new icons
    Add icons for "skills" and "language".
    Optimize the raw code inside svg files.

--- Resume_Builder/src/components/Icons.jsx
+++ Resume_Builder/src/components/Icons.jsx
@@ -3,15 +3,22 @@ import phone from '../assets/phone.svg'
 import location from '../assets/location.svg'
 import website from '../assets/website.svg'
 
+const contactIcons = { email, phone, location, website };
+
+/* section icons use raw svg code for dynamic color control.
+"skills" and "language" shouldn't be controlled but to save
+some extra logic their color is just hardcoded in the files */
+
+import skillsRaw from '../assets/skills.svg?raw'
+import languageRaw from '../assets/language.svg?raw'
+
 import profileRaw from '../assets/profile.svg?raw'
 import careerRaw from '../assets/career.svg?raw'
 import detailsRaw from '../assets/details.svg?raw'
 
-const contactIcons = { email, phone, location, website };
-// use the same for sectionIcons if you need img src
-// in that case import files instead of raw), capitalize
-
 const sectionIcons = {
+    Skills: (props) => <SvgIcon svg={skillsRaw} {...props} />,
+    Language: (props) => <SvgIcon svg={languageRaw} {...props} />,
     Profile: (props) => <SvgIcon svg={profileRaw} {...props} />,
     Career: (props) => <SvgIcon svg={careerRaw} {...props} />,
     Details: (props) => <SvgIcon svg={detailsRaw} {...props} />,
@@ -22,4 +29,4 @@ const SvgIcon = ({ svg, style }) => {
         dangerouslySetInnerHTML={{ __html: svg }}
     />; // inject raw svg code
 };
-export {contactIcons, sectionIcons}
+export { contactIcons, sectionIcons }
