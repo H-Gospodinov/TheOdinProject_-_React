c1ff233877795f853b9706d583af3b18c668fa08
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-09-03 23:50:16 +0300

    Shopping Cart: set Home Page
    Start adding content to Home Page (work in progress).

--- Shopping_Cart/src/components/Home.jsx
+++ Shopping_Cart/src/components/Home.jsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom'
+import banner from '../assets/banner.jpg'
 
 function HomePage() {
 
     return (
         <>
-            <p>HOME PAGE</p>
+            <section className="section">
+                <h1 className="section-title">Shopping Cart Demo</h1>
+                <div className="big-banner">
+                    <Link to="/about">
+                        <img src={banner} alt="" width="1600" height="500" />
+                    </Link>
+                </div>
+            </section>
+            <section className="section">
+                <h2 className="section-title">Featured Products</h2>
+            </section>
         </>
     )
 }
