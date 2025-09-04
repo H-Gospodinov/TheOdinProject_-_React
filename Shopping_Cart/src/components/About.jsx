c286ccdc1bbf373fc00bc057236c1967cedbc41d
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-09-04 15:16:25 +0300

    Shopping Cart: set About Page
    Add sample content to "About" page.

--- Shopping_Cart/src/components/About.jsx
+++ Shopping_Cart/src/components/About.jsx
@@ -1,10 +1,15 @@
+import icon from '../assets/organic.png'
 
 function AboutPage() {
 
     return (
-        <>
-            <p>ABOUT PAGE</p>
-        </>
+        <div className="demo">
+            <img src={icon} width="150" height="150" className="icon" alt="" />
+            <p className="text">Just a demo of a single-page Shopping Cart application, not a real online food store. Thanks for visiting.</p>
+            <p className="text">Check source code on
+                <a href="https://github.com/H-Gospodinov/TheOdinProject_-_React" target="_blank" rel="noopener">GitHub</a>
+            </p>
+        </div>
     )
 }
 export default AboutPage
