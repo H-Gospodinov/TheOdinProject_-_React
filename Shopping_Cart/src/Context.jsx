4debf1d291741e392001bf8d28f044d9d0fc2b74
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-09-06 00:01:59 +0300

    Shopping Cart: improve mapping
    Optimize products mapping for better performance.

--- Shopping_Cart/src/Context.jsx
+++ Shopping_Cart/src/Context.jsx
@@ -7,24 +7,23 @@ const images = import.meta.glob('./assets/images/*.{jpg,webp}',
     { eager: true, query: '?url', import: 'default' }
 ); // import images (Vite)
 
-const products = productsData.map(product => {
-
-    const image = Object.entries(images).find(([path]) =>
-        path.includes(`${product.id}.`)
-    );
-    return {
-        ...product, image: image ? image[1] : noImage
-    };
-}).sort(() => Math.random() - 0.5);
+const imageMap = Object.fromEntries(
+    Object.entries(images).map(([path, url]) => [
+        path.split('/').pop().split('.')[0], url
+    ])
+);
+const products = productsData.map(product => ({
+    ...product,
+    image: imageMap[product.id] || noImage,
+})).sort(() => Math.random() - 0.5);
 
 const ProductsContext = createContext([]);
 
 function ProductsProvider({ children }) {
-
     return (
         <ProductsContext.Provider value={products}>
             {children}
         </ProductsContext.Provider>
     );
 }
-export { ProductsProvider, ProductsContext }
+export { ProductsContext, ProductsProvider }
