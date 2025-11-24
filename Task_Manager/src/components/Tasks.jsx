fea017483fd0f5bb87855107de7c1508920b8e46
Hristo Gospodinov <177312400+H-Gospodinov@users.noreply.github.com>
2025-11-24 19:55:32 +0200

    Task Manager: minor improvements
    Remove outdated css file link.
    Fix a bug with priority filtering.

--- Task_Manager/src/components/Tasks.jsx
+++ Task_Manager/src/components/Tasks.jsx
@@ -5,9 +5,9 @@ function TaskList({ tasks, filter, date, onEdit, onArchive, onRemove }) {
     const displayTasks = (() => {
         switch (filter.name) {
             case 'home': return tasks;
-            case 'important': return tasks.filter(task => task.priority === 'high');
+            case 'important': return tasks.filter(task => task.priority === true);
             case 'upcoming': return tasks.filter(task => task.dueDate >= date);
-            case 'overdue': return tasks.filter(task => task.dueDate ? task.dueDate < date : true);
+            case 'overdue': return tasks.filter(task => task.dueDate ? task.dueDate < date : false);
             case 'archive': return tasks.filter(task => task.completed === true);
             case 'label': return tasks.filter(task => task.label === filter.text);
             default: return tasks;
